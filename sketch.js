9e92a168e3167fd0183817a78d6ffbbe9c953387
kserp <94908418+kserp@users.noreply.github.com>
2025-10-03 10:14:03 +0200

    invert text, scheduler timing for audio

--- sketch.js
+++ sketch.js
@@ -6,17 +6,20 @@ let soundSketch = (p) => {
   let sineLow, sineHigh, lowEnv, highEnv;
   let lowDistortion, highReverb, burstReverb;
   let bgNoise, bgNoiseFilter;
-  let lastHighNote = 0, lastClick = 0;
-  let clickStep = 0;
-  let inRun = false, runStep = 0, lastRunClick = 0;
+  let lastHighNote = 0;
+  let inRun = false, runStep = 0;
   let doubleSpeed = false, doubleSpeedEnd = 0;
-  let lastBurstTime = 0, burstInterval = 1200;
 
   let masterBus, masterEQ, masterComp;
   let kickClickNoise, kickClickFilter, kickClickEnv;
+  let intervalID;
+  let BPM = 140;
+  let stepCount = 0;
 
   p.setup = function () {
-    // No canvas: audio only
+    let hiddenCanvas = p.createCanvas(1, 1);
+    hiddenCanvas.hide();
+
     masterBus = new p5.Gain();
     masterEQ = new p5.EQ(3);
     masterComp = new p5.Compressor();
@@ -32,7 +35,8 @@ let soundSketch = (p) => {
     noise.disconnect(); noise.connect(bandpass);
     distortion = new p5.Distortion(0.1, '4x'); bandpass.connect(distortion); distortion.connect(masterBus);
 
-    highReverb = new p5.Reverb(); burstReverb = new p5.Reverb();
+    highReverb = new p5.Reverb();
+    burstReverb = new p5.Reverb();
 
     bgNoise = new p5.Noise('white'); bgNoise.amp(0.02); bgNoise.start();
     bgNoiseFilter = new p5.BandPass(); bgNoiseFilter.freq(900); bgNoiseFilter.res(5);
@@ -64,48 +68,59 @@ let soundSketch = (p) => {
     kickClickEnv = new p5.Envelope(); kickClickEnv.setADSR(0.001,0.005,0,0.01); kickClickEnv.setRange(0.2,0);
 
     document.addEventListener("visibilitychange",()=>{ if(document.hidden){bgNoise.amp(0,0.5);} else{bgNoise.amp(0.02,1);} });
-    lastBurstTime = p.millis();
+
+    let interval = (60000 / BPM) / 4;
+    intervalID = setInterval(playStep, interval);
   };
 
-  p.draw = function () {
-    let BPM=140; let clickInterval=(60000/BPM)/4;
-    if(p.millis()-lastClick>clickInterval){
-      clickFilter.freq(p.random(1000,3500));
-      clickFilter.res(p.random(3,8));
-      clickPanner.set(p.random(-0.7,0.7),0,0);
-      clickEnv.play(clickOsc);
-      if(p.random()<0.1){ setTimeout(()=>clickEnv.play(clickOsc),40); }
-      if(clickStep%6===0||clickStep%4===3){ triggerLowKick(0.6); } else if(clickStep%8===6&&p.random()<0.4){ triggerLowKick(0.1); }
-      if(!inRun&&p.random()<0.02){ inRun=true; runStep=0; }
-      if(!doubleSpeed&&p.random()<0.01){ doubleSpeed=true; doubleSpeedEnd=p.millis()+p.random(800,1500); }
-      clickStep++; lastClick=p.millis();
+  p.draw = function () {};
+
+  function playStep() {
+    clickFilter.freq(p.random(1000, 3500));
+    clickFilter.res(p.random(3, 8));
+    clickPanner.set(p.random(-0.7, 0.7), 0, 0);
+    clickEnv.play(clickOsc);
+    if (Math.random() < 0.1) setTimeout(() => clickEnv.play(clickOsc), 40);
+
+    if (stepCount % 6 === 0 || stepCount % 4 === 3) {
+      triggerLowKick(0.6);
+    } else if (stepCount % 8 === 6 && Math.random() < 0.4) {
+      triggerLowKick(0.1);
     }
 
-    if(p.millis()-lastBurstTime>burstInterval){
-      if(p.random()<0.5){ burstEnv.play(noise);
-        burstReverb.process(noise,0.1,2); burstReverb.drywet(0.2); burstReverb.connect(masterBus);
-      }
-      burstInterval=p.random(800,1600); lastBurstTime=p.millis();
+    if (!inRun && Math.random() < 0.02) { inRun = true; runStep = 0; }
+    if (inRun) {
+      hiClickFilter.freq(p.random(8000, 10000));
+      hiClickEnv.play(hiClickOsc);
+      runStep++; if (runStep >= 16) inRun = false;
     }
 
-    if(doubleSpeed&&p.millis()>doubleSpeedEnd){ doubleSpeed=false; }
+    if (!doubleSpeed && Math.random() < 0.01) {
+      doubleSpeed = true; doubleSpeedEnd = p.millis() + p.random(800, 1500);
+    }
+    if (doubleSpeed && p.millis() > doubleSpeedEnd) doubleSpeed = false;
 
-    if(p.millis()-lastHighNote>1700){
-      let baseFreq=698.46; if(p.random()<0.15) baseFreq=659.25; sineHigh.freq(baseFreq);
-      if(p.random()<0.5){ highEnv.setADSR(0.01,0.05,0,0.1); highEnv.setRange(0.2,0); }
+    if (Math.random() < 0.2) {
+      burstEnv.play(noise);
+      burstReverb.process(noise,0.1,2); burstReverb.drywet(0.2); burstReverb.connect(masterBus);
+    }
+
+    if (p.millis() - lastHighNote > 1700) {
+      let baseFreq = 698.46; if (Math.random() < 0.15) baseFreq = 659.25;
+      sineHigh.freq(baseFreq);
+      if (Math.random() < 0.5) { highEnv.setADSR(0.01,0.05,0,0.1); highEnv.setRange(0.2,0); }
       else { highEnv.setADSR(0.02,0.2,0,0.4); highEnv.setRange(0.3,0); }
-      highEnv.play(sineHigh); lastHighNote=p.millis();
+      highEnv.play(sineHigh); lastHighNote = p.millis();
     }
+    stepCount++;
+  }
 
-    if(inRun&&p.millis()-lastRunClick>50){
-      hiClickFilter.freq(p.random(8000,10000)); hiClickEnv.play(hiClickOsc);
-      runStep++; lastRunClick=p.millis(); if(runStep>=16) inRun=false;
-    }
-  };
-
-  function triggerLowKick(vol){
-    lowEnv.setADSR(0.01,0.01,0.7,0.2); lowEnv.setRange(3,0); lowEnv.play(sineLow);
+  function triggerLowKick(vol) {
+    lowEnv.setADSR(0.01,0.01,0.7,0.2);
+    lowEnv.setRange(3,0);
+    lowEnv.play(sineLow);
     lowDistortion.set(p.random(0.0,0.003),'2x');
-    kickClickFilter.freq(p.random(2500,5000)); kickClickEnv.play(kickClickNoise);
+    kickClickFilter.freq(p.random(2500,5000));
+    kickClickEnv.play(kickClickNoise);
   }
 };
